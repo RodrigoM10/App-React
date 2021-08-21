7744da06de3aa15458595a1b3c4489988fe9be2b
Rodrigo <rodrigomendozar10@gmail.com>
2021-08-21 20:30:50 -0300

    optimizacion de codigo, no se ve la imagen

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,30 @@ import { NavRB } from './components/TheNav';
 import { Container } from 'react-bootstrap';
 import { Productos } from './components/Productos';
 
-
-
 function App() {
 
   let title = " Productos destacados de verano"
-  let title1 = " Productos destacados de Invierno"
 
-  let producto = {
-    id: '1',
-    nombre: 'pantalon verde',
-    imagen: 'https://tse1.mm.bing.net/th?id=OIP.rTYfHDrTyYL7U2Dl8S0gTAAAAA&pid=Api&P=0&w=300&h=300'
-  }
-  let producto1 = {
-    id: '2',
-    nombre: 'pantalon azul',
-    imagen: 'https://tse2.mm.bing.net/th?id=OIP.i9r0JTzi3Bf93FwpsLiUGAHaJ4&pid=Api&P=0&w=300&h=300'
-  }
+  let productos = [
+     {
+      id: '1',
+      nombre: 'Pantalon verde',
+      img: 'https://tse1.mm.bing.net/th?id=OIP.rTYfHDrTyYL7U2Dl8S0gTAAAAA&pid=Api&P=0&w=300&h=300'
+    },
+    {
+      id: '2',
+      nombre: 'Pantalon azul',
+      img: 'https://tse2.mm.bing.net/th?id=OIP.i9r0JTzi3Bf93FwpsLiUGAHaJ4&pid=Api&P=0&w=300&h=300'
+    }
+  ]
   return (  
     <div>
       <NavRB />
       <Container>
       <h1>Hola Mundo</h1>
       {/* <Contador /> */}
-      <Productos title={title} producto={producto} />
-      <Productos title={title1} producto={producto1} />
+      <Productos title={title} productos={productos} />
+      {/* <Productos title={title1} producto={producto1} /> */}
       </Container>
     </div>
   );
--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,41 +1,19 @@
-import React from 'react'
-import { Card, Button } from 'react-bootstrap'
+import React from 'react';
+import { Producto } from './Producto';
 
-export const Productos = ({title, producto}) => {
+export const Productos = ({ title, productos }) => {
+    const mapProductos = productos.map((producto) => (
+                   <Producto 
+                   producto={producto} />
+                ));
 
-    const {nombre, imagen} = producto;
     return (
         <>
-        <h2>{title}</h2>  
-        <div className="d-flex flex-wrap justify-content-between">
-        <Card style={{ width: '15rem' }}>
-        <Card.Img 
-        variant="top"
-        src={imagen}
-         />
-        <Card.Body>
-          <Card.Title>{nombre}</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      
-      <Card style={{ width: '15rem' }}>
-        <Card.Img variant="top" src="https://tse1.mm.bing.net/th?id=OIP.rTYfHDrTyYL7U2Dl8S0gTAAAAA&pid=Api&P=0&w=300&h=300" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      </div>
-      </>
-    )
-}
+            <h2>{title}</h2>
+            <div className="d-flex flex-wrap justify-content-between">
+                {mapProductos}
+            </div>
+        </>
+    );
+};
 
